27671e6a82f29421dbc1c384f9a85a638948455a
scharlez <stevencharlez@gmail.com>
2022-07-04 22:16:41 -0700

    Complete Week 3 Assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -15,42 +17,147 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+// describe("arrayFibz", () => {
+//     it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+//         const fibLength1 = 6
+//         // // Expected output: [1, 1, 2, 3, 5, 8]
+//         const fibLength2 = 10
+//         // // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+//         expect(arrayFibz(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//         expect(arrayFibz(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+//     })
+// })
 
+//* RAN TEST (RESULTS BELOW): *//
+
+// ReferenceError: arrayFibz is not defined 
 
 // b) Create the function that makes the test pass.
 
+//Pseudocode:
+// Declare a function called arrayOfFibz. 
+// Set the parameter as an array called arrayFibonnaci. 
+// Use for loop to that takes in a number (greater than 2)
+// Return array with Fib sequence
+
+
+// const arrayFibz = (arrayFibonnaci) => {
+//     for (i =0; i<arrayFibonnaci.length, i++) {
+//         if (i > 2) {
+//             arrayFibonnaci[i] = array [i - 1] + arrayFibonnaci [i -2]; 
+//         } else {
+//             arrayFibonnaci[i] = 1;
+//         }
+//         return arrayFibonnaci
+//     }
+    
+//     // arrayFibonnaci.push(arrayFibonnaci[i-2] + arrayFibonnaci[i-1])
+//     // return i 
+
+// }
+
+// ** NOTES TO INSTRUCTOR **: I was getting a SyntaxError on Line 46 and I'm not sure why, Jest suggested a semicolon but that did nothing and seemed odd but honestly I'm not even sure the code would have worked without the error...still processing the logic and basics to be frank. (this is why my code above is commented out.)
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
+describe("leastThenGreater", () => {
+    it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+        const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+        // Expected output: [-9, 7, 9, 199]
+        const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+        // Expected output: [-823, 7, 23]
+        expect(leastThenGreater([4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"])).toEqual([-9, 7, 9, 199])
+        expect(leastThenGreater(["hello", 7, 23, -823, false, 78, null, "67", 6, "number"])).toEqual([-823, 7, 23])
 
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
+    })
+})
 
+//* RAN TEST (RESULTS BELOW): *//
+
+// ReferenceError: leastThenGreater is not defined
 
 // b) Create the function that makes the test pass.
 
+//Pseudocode:
+// Declare a function called leastThenGreater
+// Set the parameter as an array called oddNums
+// Use the .filter method to filter odd numbers
+// Use the .sort method to sort data from least to greatest
+// Use the .map to return expectd results.
+
+// const leastThenGreater = (oddNums) => {
+//     return oddNums.filter (number => number %2 !==0) + oddNums.sort((a,b) => a-b) + oddNums.map (number => number %2 !==0)
+// }
+
+
+//* RAN TEST (RESULTS BELOW): *//
+
+
+// FAIL  ./code-challenges.test.js
+// leastThenGreater
+//   ✕ takes in an array and returns a new array of only odd numbers sorted from least to greatest (2 ms)
+
+// ● leastThenGreater › takes in an array and returns a new array of only odd numbers sorted from least to greatest
+
+//   expect(received).toEqual(expected) // deep equality
+
+//   Expected: [-9, 7, 9, 199]
+//   Received: "9,7,true,hey,7,199,-9,hola-9,0,false,true,4,7,7,8,9,hey,199,holatrue,false,false,true,false,true,true,false,true,true,true,true"
+
+// ** NOTES TO INSTRUCTOR **: I'm not sure what the Jest test result was saying I did incorrectly, I found the response odd/confusing. Any clarity on this matter? 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+describe("onlySum", () => {
+    it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+        const numbersToAdd1 = [2, 4, 45, 9]
+        // Excpected output: [2, 6, 51, 60]
+        const numbersToAdd2 = [0, 7, -8, 12]
+        // Expected output: [0, 7, -1, 11]
+        const numbersToAdd3 = []
+        // Expected output: []
+        expect(onlySum([2, 4, 45, 9])).toEqual([2, 6, 51, 60])
+        expect(onlySum([0, 7, -8, 12])).toEqual([0, 7, -1, 11])
+        expect(onlySum([])).toEqual([])
+    })
+})
 
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
+//* RAN TEST (RESULTS BELOW): *//
 
-const numbersToAdd3 = []
-// Expected output: []
-
+// ReferenceError: onlySum is not defined
 
 // b) Create the function that makes the test pass.
+
+// Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+//Pseudocode:
+// Declare a function called onlySum. 
+// Set the parameter as an array.
+// Use the .forEach method accumlate sum of array elements. 
+// Use the .map to return expectd results.
+
+const onlySum = (sumArray) => {
+    let sum = 0;
+    return sumArray.forEach(value => 
+        sum+= value) + sumArray.map 
+    
+}
+
+//* RAN TEST (RESULTS BELOW): *//
+
+// FAIL  
+// onlySum
+//     ✕ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array. (1 ms)
+// ● onlySum › takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+// expect(received).toEqual(expected) // deep equality
+
+// Expected: [2, 6, 51, 60]
+// Received: "undefinedfunction map() { [native code] }"
+
+// ** NOTES TO INSTRUCTOR **: Get a similiar issue to the one before, I'm guessing my logic is a bit off somehow? 
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,58 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: `This` is for functionality, so when you are using react for instance and you want to apply certain behaviors to an object or a component you could type this.state or this.xyzMethod to change how the outputs or results are being managed.
 
-  Researched answer:
-
+  Researched answer: `This` allows programmers to use/reuse functions with different contexts, meaning it tell which object will be the main focus when invoking a function. `This` keyword can be used in a variety of ways depending upon the goal. It can used to reference current class instance variables, as a method parameter, or most popularly to invoke particular class method/as an agrument in the constructor call as seen in React. 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is an interactive library for building user interfaces within the Javascript language environment. It is a useful tool to help mainly front-end developers streamline tasks dealing with customizing aspects geared toward to the users' experience. React also collaborates well with HTML & CSS languages becauses they all target the same audience - user interface. 
 
-  Researched answer:
+  Researched answer: React is a Javascript library, originally founded by Facebook, but mostly seen as a Javascript framework for building user interfaces components. React is seen as primarily a front-end developer tool as it collaborates well with the HTML & CSS languages becauses they all target the same audience - UI.  It is dynamic in nature due to it giving the developer the ability to define components as classes or functions and changes only what actually needs changing. It has lifecycles that are used for initializing thru using: `constructor()` along with `super()` - in tandem with the `this.state` & event handler methods; as well as lifecycles for updating like: `render()`, which is the only required method when it comes to class components but its a bit picky as one cannot do any sort of modifications in this stage so that must be done elsewhere. 
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: `this.setState` is the method required during a React class component because it initializes or starts the behaviors or how it will interact with users once the programming is complete. Additionally it is used to update the information of component. 
 
-  Researched answer:
+  Researched answer: The `render()` is lifecycle method that is most common and required when it comes to a React class component due to it handling the performance of the component in relation to the UI. React is conditional with the use of `render()`, meaning the functions following it must return with the same output when the same inputs are inserted. Therefore,  `this.setState` cannot be used under a render since you don't (or can't) modify the component state from this level. 
 
 
 
 4. What is JSX? What is one syntax difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is used with React and responsible for those familiar with HTML syntax to do similiar work through the Javascript environment. With JSX, one has to import or point the child components back to the parent edition on the original App.js file which stores the bulk of the information. 
 
-  Researched answer:
+  Researched answer: JSX is JavaScript + XML or a React version of HTML, allows the use of HTML-like syntax that can be used to create React components. Without JSX, it would more difficult to create largely nested HTML documents using JS only syntax. Speaking of syntax, a major difference between native HTML and JSX is with JSX the word "class" cannot be used to identify HTML/CSS classes, since that word is reserved in JavaScript, so as an alternative or work around, instead one would use - "className". 
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a JS file manager that is used to connect your JS files to a node so that one's changes can be viewed in a browser. When you run yarn your App.js and node files are modified becauses they are directly effected by the changes or edits done when writing code through React/JS. 
 
-  Researched answer:
+  Researched answer: Yarn is package file manager, originally released from Facebook that is used with React and Jest. When running yarn through a React application the "package.json" and "yarn.lock" files are modified because are both important pieces for connecting the node and active files. 
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: 
 
   Researched answer:
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: applying conditions in the same manner you would with regular Javascript like using conditional operators or JS operators like `if` to create live element representation that thru React can be udated to mirror the user interface.
 
-2. Object-oriented programming:
+2. Object-oriented programming: centered around object creation that contains data & methods, this helps lessen repetition of code and thus cleaner.
 
-3. Ruby:
+3. Ruby: open source pprogramming language thats designed to be more humancenteric, meaning the syntax is mire natural to read and easy to write having a focus around simpliicity yet pproductivity. 
 
-4. Ruby blocks:
+4. Ruby blocks: allows programmers more freedom and flexible when it comes to code because the functions are annoymous in nature and can be passed into methods.
 
-5. Ruby hashes:
+5. Ruby hashes: aka associative arrays is a collection of individualized keys and their relative values, the layout is like an array but instead of having integers, programmers have the ability to use any object type as its index.
